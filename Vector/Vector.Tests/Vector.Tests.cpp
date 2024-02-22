4e5328b1fbbdc23f41680bfa2cbd3bbc43a880f7
PylaevaSvetlana <pylaeva.sweta@yandex.ru>
2024-02-22 10:32:57 +0300

    Добавлены зависимости между проектами

---
+++ Vector/Vector.Tests/Vector.Tests.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VectorTests
+{
+	TEST_CLASS(VectorTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
